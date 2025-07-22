83f15fedd98a2d19f4ab018dbfe9ce7a563bed6b
Olicpp <124665408+Olicpp@users.noreply.github.com>
2025-07-22 17:46:36 +0200

    Initial commit

---
+++ src/main/java/dolce/example/DolceAmore/DolceAmoreApplication.java
@@ -0,0 +1,13 @@
+package dolce.example.DolceAmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolceAmoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolceAmoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dolce/example/DolceAmore/DolceAmoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dolce.example.DolceAmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolceAmoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
